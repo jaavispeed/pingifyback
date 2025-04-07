41cc9ba719f5ae9216d34d25225247336d75f40d
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-04-07 17:02:24 -0400

    Password bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import * as bcrypt from  'bcrypt'
 
 
 @Injectable()
@@ -15,8 +16,11 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
     try {
-      const user = this.userRepository.create(createUserDto);
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({...userData, password: bcrypt.hashSync(password, 10) });
       await this.userRepository.save(user);
+      delete user.password; // Remove password from the response
       return user;
 
     } catch (error) {
