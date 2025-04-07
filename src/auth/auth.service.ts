a842bec1bf14e67e7ffdef1cfc77f3daaf259327
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-04-07 17:48:42 -0400

    JWT OK

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,8 +3,10 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
-import * as bcrypt from  'bcrypt'
+import * as bcrypt from 'bcrypt'
 import { LoginUserDto } from './dto/login-user.dto';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 
 @Injectable()
@@ -13,16 +15,20 @@ export class AuthService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
   ) { }
 
   async create(createUserDto: CreateUserDto) {
     try {
       const { password, ...userData } = createUserDto;
 
-      const user = this.userRepository.create({...userData, password: bcrypt.hashSync(password, 10) });
+      const user = this.userRepository.create({ ...userData, password: bcrypt.hashSync(password, 10) });
       await this.userRepository.save(user);
       delete user.password; // Remove password from the response
-      return user;
+      return {
+        ...user,
+        token: this.getJwtToken({ email: user.email }),
+      };
 
     } catch (error) {
       this.handleDBErrors(error);
@@ -30,26 +36,28 @@ export class AuthService {
     }
   }
 
-
-  async login (loginUserDto: LoginUserDto) {
-
+  async login(loginUserDto: LoginUserDto) {
     try {
       const { email, password } = loginUserDto;
       const user = await this.userRepository.findOne({ where: { email }, select: { email: true, password: true } });
 
       if (!user) throw new UnauthorizedException('Credentials are not valid (email)');
-
       if (!bcrypt.compareSync(password, user.password)) throw new UnauthorizedException('Credentials are not valid (password)');
 
-      return user;
-      //RETORNAR JWT
+      return {
+        ...user,
+        token: this.getJwtToken({ email: user.email }),
+      };
 
     } catch (error) {
       this.handleDBErrors(error);
     }
-
   }
 
+  private getJwtToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
+  }
 
   private handleDBErrors(error: any): never {
     if (error.code === '23505')
---
+++ src/auth/interfaces/jwt-payload.interface.ts
@@ -0,0 +1,8 @@
+
+
+export interface JwtPayload {
+
+    email: string;
+
+    // Add other properties as needed
+}
