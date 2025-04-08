70f8f29fba246f2fbfc5330c3b0c05bcf3deb5d7
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-04-08 18:28:07 -0400

    Roles OK

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from './entities/user.entity';
+import { RawHeaders } from './decorators/raw-headers.decorator';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
+import { RoleProtected } from './decorators/role-protected/role-protected.decorator';
+import { ValidRoles } from './interfaces/valid-roles';
 
 @Controller('auth')
 export class AuthController {
@@ -20,11 +26,32 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard())
-  TestingPrivateRoute(){
+  TestingPrivateRoute(
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+
+    @RawHeaders () rawHeaders: string[]
+  ){
     return {
       ok: true,
       message: 'hola',
-      user: {name: 'asd'}
+      user,
+      userEmail,
+      rawHeaders
+    }
+  }
+
+
+  // @SetMetadata('roles', ['admin'])
+  @Get('private2')
+  @RoleProtected(ValidRoles.admin)
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  privateRoute2(
+    @GetUser() user: User
+  ){
+    return{
+      ok: true,
+      user
     }
   }
 
