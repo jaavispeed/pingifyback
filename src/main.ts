43c22720796d780193278935bf18eec774f78434
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-04-08 21:24:31 -0400

    Habilitacion de corse

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
 
+    // Configuración global de CORS
+    app.enableCors({
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+       allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-Control-Allow-Credentials, Authorization',
+      credentials: true,
+    });
+
+
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(new ValidationPipe({
