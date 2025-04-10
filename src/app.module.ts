327435ce7e10b0f74430f9193a3d3554b5721af4
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-04-09 23:38:58 -0400

    instalacion de openai

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { AiModule } from './ai/ai.module';
 
 
 @Module({
@@ -18,6 +19,7 @@ import { AuthModule } from './auth/auth.module';
       synchronize: true,
     }),
     AuthModule,
+    AiModule,
   ],
 })
 export class AppModule {}
